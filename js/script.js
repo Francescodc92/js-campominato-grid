40f95d6f1659e4a3d03df398aeaaa4ba115125d5
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-21 16:22:57 +0200

    end exercize start bonus 1

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 
 RAGIONAMENTO BASE
 1. prendere il bottone che da inizio alla partita 
+    nel caso prendo il form e agiungo l'evento al sumbit 
 2. intercettare il click 
 3. quando l'utente cliccherà il bottone 
-  -creare un ciclo che definisce quante volte creare l'elemento (di conseguenza definisce quandi elementi creeremo nella pagina)
+  -creare un ciclo che definisce quante volte creare l'elemento (di conseguenza definisce quandi elementi che creeremo nella pagina)
   - chiamare la funzione che crea gli elementi in pagina ad ogni giro
 
 4. creare una funzione che crea l'elemento html da agiungere alla pagina 
@@ -16,3 +17,32 @@ RAGIONAMENTO BASE
 5. creare una funzione legata al click dell'utente che cambia il bakground dell'elemento e scrive in console l'innerhtml dell'elemento 
 
 */
+const formSettingsGame = document.getElementById("game-settings")
+const gameContainer = document.querySelector(".game-container")
+const cellsNumber = 100
+const createElementHTML = (index) => {
+  const htmlCellContainer = document.createElement("div")
+  htmlCellContainer.classList.add("game-cell-container")
+  const htmlCellElement = document.createElement("div")
+  htmlCellElement.classList.add("game-cell")
+  htmlCellElement.innerHTML = index
+  htmlCellElement.addEventListener("click", cellClicched)
+  htmlCellContainer.append(htmlCellElement)
+
+  return htmlCellContainer
+}
+const cellClicched = (e) => {
+  e.target.classList.add("clicked")
+  console.log(e.target.innerHTML)
+}
+
+function gameStart(e) {
+  e.preventDefault()
+  gameContainer.innerHTML = ""
+  for (let i = 1; i <= cellsNumber; i++) {
+    const createdElement = createElementHTML(i)
+    gameContainer.append(createdElement)
+  }
+}
+
+formSettingsGame.addEventListener("submit", gameStart)
